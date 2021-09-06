852f85cce6279b8d38fc4fb727692c3911c2181a
Bernhard Molz <molzb2@gmx.de>
2021-09-06 23:56:58 +0200

    Cache-Control, UI

--- src/main/java/com/ing/dbcompare/util/Http.java
+++ src/main/java/com/ing/dbcompare/util/Http.java
@@ -6,11 +6,15 @@ import static java.net.HttpURLConnection.HTTP_OK;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 import com.sun.net.httpserver.HttpExchange;
 
 public class Http {
+    private static List<String> cachedFilePattern = Arrays.asList(new String[] { "woff2" });
+
     private static Map<String, String> contentTypes = //
             Map.of("html", "text/html; charset=utf-8", "js", "application/javascript; charset=utf-8", "json",
                     "application/json; charset=utf-8", "css", "text/css", "png", "image/png");
@@ -22,11 +26,27 @@ public class Http {
     public static void response(HttpExchange t, int statusCode, byte[] content) throws IOException {
         String path = t.getRequestURI().getPath();
         String suffix = path.substring(path.lastIndexOf('.') + 1);
+        addCacheControlHeader(t, path);
         t.getResponseHeaders().add("Content-Type", Http.getContentTypeFor(suffix));
-
         t.sendResponseHeaders(statusCode, content.length);
-        try (OutputStream os = t.getResponseBody()) {
-            os.write(content);
+
+        // if (path.contains("packed")) { // GZIP compression
+        //     t.getResponseHeaders().add("Content-Encoding", "gzip");
+        //     try (OutputStream os = new GZIPOutputStream(t.getResponseBody())) {
+        //         os.write(content);
+        //     }
+        // } else { // no compression
+            try (OutputStream os = t.getResponseBody()) {
+                os.write(content);
+            // }
+        }
+    }
+
+    private static void addCacheControlHeader(HttpExchange t, String path) {
+        for (String ptn : cachedFilePattern) {
+            if (path.contains(ptn)) {
+                t.getResponseHeaders().add("Cache-Control", "max-age=31536000");
+            }
         }
     }
 
@@ -36,14 +56,14 @@ public class Http {
     }
 
     public static void serveFile(HttpExchange t) throws IOException {
-		String path = t.getRequestURI().getPath(); // e.g. /db/select.json...
-		String file = path.substring(3); // remove '/db' from URL
-		try (InputStream is = Http.class.getResourceAsStream(file)) {
-			if (is == null) {
-				Http.response(t, HTTP_NOT_FOUND, "File not found: " + t.getRequestURI());
-			} else {
-				Http.response(t, HTTP_OK, is.readAllBytes());
-			}
-		}
-	}
+        String path = t.getRequestURI().getPath(); // e.g. /db/select.json...
+        String file = path.substring(3); // remove '/db' from URL
+        try (InputStream is = Http.class.getResourceAsStream(file)) {
+            if (is == null) {
+                Http.response(t, HTTP_NOT_FOUND, "File not found: " + t.getRequestURI());
+            } else {
+                Http.response(t, HTTP_OK, is.readAllBytes());
+            }
+        }
+    }
 }
